a4b6f98f0067a4c4653b5c67a9e3ef5771197a48
JairoDuenas <jairoduenas.ing@gmail.com>
2022-08-01 15:28:30 -0500

    Contacts y Actions

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -3,18 +3,20 @@ import { ref, reactive, computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import MessageItem from '@/components/MessageItem.vue'
 import useMessagesStore from '@/stores/messages.js'
+import useContactsStore from '@/stores/contacts.js'
 
 const route = useRoute()
 const messagesStore = useMessagesStore()
+const contactsStore = useContactsStore()
 
 const end = ref(null)
 const channelId = ref(null)
 const title = ref('')
 const people = reactive([])
-const message = reactive([])
+const message = ref('')
 
 const messagesView = computed(() => messagesStore.findMessagesByChannelId(channelId.value).map((message) => {
-  const author = people.find((p) => p.id === message.author)
+  const author = contactsStore.getContactById(message.author)
   if (!author) return message;
     return {
     ...message,
@@ -22,6 +24,11 @@ const messagesView = computed(() => messagesStore.findMessagesByChannelId(channe
     self: author.id === 1
   }
 }))
+
+const addMessage = () => {
+  messagesStore.addMessage(channelId.value, message.value)
+  message.value = ''
+}
 
 const scrollToBottom = () => {
   end.value?.scrollIntoView({
@@ -67,12 +74,12 @@ scrollToBottom()
       />
       <span ref="end"></span>
     </div>
-    <footer>
-      <textarea rows="3"></textarea>
+    <form class="footer" @submit.prevent="addMessage" >
+      <textarea rows="3" v-model="message" ></textarea>
       <button>
         <Icon icon="carbon:send-alt" />
       </button>
-    </footer>
+    </form>
   </div>
 </template>
 
@@ -97,7 +104,7 @@ scrollToBottom()
   .content {
     @apply flex flex-col gap-4 p-4 h-full overflow-y-auto;
   }
-  footer {
+  .footer {
     @apply flex p-2;
     textarea {
       @apply w-full px-2 py-2 resize-none bg-zinc-800 rounded-tl-md rounded-bl-md focus:outline-none;
